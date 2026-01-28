6f820ea74a7b24abff5715bce5ee6247182a787d
murtaza <m.murtaza,ahmadi78@gmail.com>
2026-01-28 17:31:12 +0430

    edit

--- backend/src/server.js
+++ backend/src/server.js
@@ -54,13 +54,13 @@ app.get("/health", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+//   app.use((req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+//   });
+// }
 
 connectDB().then(() => {
   app.listen(PORT, () => {
